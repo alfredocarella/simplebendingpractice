c1fb72dec474117ec013b541e603d3f5f8520e8c
Alfredo Carella <alfredocarella@gmail.com>
2017-04-18 16:00:01 +0200

    Add first nose test

---
+++ bending.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+class Distload(np.poly1d):
+    pass
+    # def __init__(self):
+    #     pass
+
+
+class Beam():
+    pass
---
+++ test/bending_test.py
@@ -0,0 +1,17 @@
+import unittest
+from bending import Distload
+from bending import Beam
+
+
+class TestDistload(unittest.TestCase):
+    def setUp(self):
+        ppar = [5, 4, 3, -10]
+        self.q = Distload(ppar)
+
+    def test_distload_polynomial_evaluates_to_right_value(self):
+        self.assertEqual(52, self.q(2))
+
+
+class TestBeam(unittest.TestCase):
+    def setUp(self):
+        self.my_beam = Beam(10)
