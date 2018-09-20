1314821106b3489d8f17552b53b51af622f44d52
Alfredo Carella <alfredocarella@gmail.com>
2018-09-20 16:29:35 +0200

    Add function for testing calculation of beam diagrams.

--- test/test_crunching.py
+++ test/test_crunching.py
@@ -1,4 +1,7 @@
+import numpy as np
 from numpy.testing import assert_allclose
+import sympy
+
 import crunching  # calculate_reaction_forces, plot_analytical
 
 
@@ -13,3 +16,40 @@ def test_get_reaction_forces():
     F_Ax, F_Ay, F_By = crunching.get_reaction_forces(x_coords, (FRx, FRy), M_R)
     expected = 20, 109, 41
     assert_allclose((F_Ax, F_Ay, F_By), expected, rtol=1e-5)
+
+
+def test_calculate_diagrams():
+    # User input
+    beam_span = (0, 9)
+    fixed_support = 2
+    rolling_support = 7
+    loads = [crunching.PointLoad(-20, 3),
+             crunching.DistributedLoad(-20, (0, 2)),
+             crunching.DistributedLoad("-10", (3, 9))]
+
+    x0, x1, dist_forces, shear_forces, bending_moments = crunching.calculate_diagrams(beam_span, fixed_support, rolling_support, loads)
+    x_vec = np.linspace(0, 9, 19)
+
+    x = sympy.symbols('x')
+
+    distributed_calc = sympy.lambdify(x, sum(dist_forces), "numpy")(x_vec)
+    distributed_expected = [-20]*5 + [0] + [-10]*13
+    np.testing.assert_allclose(distributed_calc, distributed_expected)
+
+    shear_calc = sympy.lambdify(x, sum(shear_forces), "numpy")(x_vec)
+    shear_expected = [0, -10, -20, -30, 36, 36, 16, 11, 6, 1, -4, -9, -14, -19, 20, 15, 10, 5, 0]
+    np.testing.assert_allclose(shear_calc, shear_expected)
+
+    bending_calc = sympy.lambdify(x, sum(bending_moments), "numpy")(x_vec)
+    print(bending_calc)
+    bending_expected = [0, -2.5, -10, -22.5, -40, -22, -4, 2.75, 7, 8.75, 8, 4.75, -1, -9.25, -20, -11.25, -5, -1.25, 0]
+    np.testing.assert_allclose(bending_calc, bending_expected)
+
+
+def test_Beam_is_correctly_created():
+    beam_span = [0, 9]
+    my_beam = crunching.Beam(beam_span)
+    assert my_beam.x0 == 0
+    assert my_beam.x1 == 9
+
+
