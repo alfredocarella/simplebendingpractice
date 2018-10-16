bbdfb223c6611ac40b24012809082ba3630358f8
Alfredo Carella <alfredocarella@gmail.com>
2018-10-15 22:04:29 -0300

    Add a first setup.py file to make the package installable

---
+++ setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""Setup file for simplebending package
+
+Developer install
+-----------------
+Run following command in Anaconda prompt/terminal:
+    pip install -e .
+"""
+from setuptools import setup
+
+
+setup(name='beambending',  # name of package on import
+      version='0.1',  # package version
+      description='Educational package for visualizing shear forces and bending moments on a beam',  # brief description
+      license='MIT',  # licensing
+      long_description=open('README.md').read(),
+      url='https://github.com/alfredocarella/simplebendingpractice',  # git repo url
+      author='Alfredo Carella',  # author(s)
+      author_email='alfcar@oslomet.no',  # email
+      packages=['beambending'],  # main package
+      install_requires=[  # dependencies
+        'matplotlib',  # for plotting
+        'numpy',  # for numerical calculations
+        'numpydoc',  # numpy-style docstrings for sphinx
+        'pytest',  # for testing
+        'pytest-cov',  # for calculating coverage
+        'sphinx',  # generating documentation
+        'sphinx_rtd_theme',  # docs theme
+        'sympy'  # for analytical integration
+      ],
+      zip_safe=True)  # package can be installed from zip file
