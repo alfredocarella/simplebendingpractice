09405fd9a22d12b758ba1ca0cc3ef0f006646dc0
Alfredo Carella <alfredocarella@gmail.com>
2019-01-23 20:48:46 +0100

    Remove lonely methods and associated tests. Leave only the Beam class.
    The Beam class was already functional, and is the preferred way.

--- test/test_crunching.py
+++ test/test_crunching.py
@@ -7,41 +7,6 @@ from beambending import beam
 from contextlib import contextmanager
 
 
-def test_get_reaction_forces():
-    x_coords = (2, 7)
-    FRx, FRy, M_R = -20, -150, -505
-    F_Ax, F_Ay, F_By = beam.get_reaction_forces(x_coords, (FRx, FRy), M_R)
-    expected = 20, 109, 41
-    assert_allclose((F_Ax, F_Ay, F_By), expected, rtol=1e-5)
-
-
-def test_calculate_diagrams():
-    # User input
-    beam_span = (0, 9)
-    fixed_support = 2
-    rolling_support = 7
-    loads = [beam.PointLoad(-20, 3),
-             beam.DistributedLoad(-20, (0, 2)),
-             beam.DistributedLoad("-10", (3, 9))]
-
-    x0, x1, dist_forces, shear_forces, bending_moments = beam.calculate_diagrams(beam_span, fixed_support,
-                                                                                 rolling_support, loads)
-    x_vec = np.linspace(0, 9, 19)
-    x = sympy.symbols('x')
-
-    distributed_calc = sympy.lambdify(x, sum(dist_forces), "numpy")(x_vec)
-    distributed_expected = [-20] * 5 + [0] + [-10] * 13
-    np.testing.assert_allclose(distributed_calc, distributed_expected)
-
-    shear_calc = sympy.lambdify(x, sum(shear_forces), "numpy")(x_vec)
-    shear_expected = [0, -10, -20, -30, 36, 36, 16, 11, 6, 1, -4, -9, -14, -19, 20, 15, 10, 5, 0]
-    np.testing.assert_allclose(shear_calc, shear_expected)
-
-    bending_calc = sympy.lambdify(x, sum(bending_moments), "numpy")(x_vec)
-    bending_expected = [0, -2.5, -10, -22.5, -40, -22, -4, 2.75, 7, 8.75, 8, 4.75, -1, -9.25, -20, -11.25, -5, -1.25, 0]
-    np.testing.assert_allclose(bending_calc, bending_expected)
-
-
 def test_beam_is_correctly_created():
     span = (0, 9)
     my_beam = beam.Beam(span)
